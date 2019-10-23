4ee92424969338bd279ca9fb9eb54a1cf0a049fa
Aaron Bae <aaronbae@uw.edu>
2019-10-22 19:12:40 -0700

    made small changes

--- lib/local_variance.m
+++ lib/local_variance.m
@@ -1,4 +1,4 @@
-function [output] = local_variance(A,t)
+function [output] = local_variance(second_derivative, zero_crossing, t)
 % Calculates the local variance of image matrix A and returns a new matrix
 % with variance value above threshold t
 % Inputs : 
@@ -7,16 +7,15 @@ function [output] = local_variance(A,t)
 %Outputs : 
 %   output: matrix output with edge detected
 
-    output = zeros(size(A));
+    output = zeros(size(second_derivative));
     
-    a = padarray(A,[1,1],0)
-    mu = conv2(A,[1 1 1;1 1 1;1 1 1], 'same')/9;
-    mu2 = conv2(A^2,[1 1 1;1 1 1;1 1 1], 'same')/9;
+    second_derivative_padded = padarray(second_derivative,[1,1],0);
+    mu = conv2(second_derivative_padded,[1 1 1;1 1 1;1 1 1], 'same')/9;
+    mu2 = conv2(second_derivative_padded.^2,[1 1 1;1 1 1;1 1 1], 'same')/9;
     
-    var = mu2 - mu^2;
+    var = mu2 - mu.^2;
+    var = var(2:size(var)-1,2:size(var)-1);
     
-    output(var>t) = 1;   
-    output = output(2:size(output)-1,2:size(output)-1)
-    
+    output(var>t & zero_crossing==1) = 1;   
 end
 
